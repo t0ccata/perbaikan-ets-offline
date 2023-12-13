81ac4adcc906a69ffa2552826f0317636b51e923
Abdurrahman Faiz <m.abdurrahman.faiz24@gmail.com>
2023-12-13 08:51:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon',
+        method: 'GET',
+        success: function (response) {
+            displayData(response.results);
+        },
+        error: function (error) {
+            console.error("Error fetching data : ", error);
+        }
+    });
+
+    function displayData(response) {
+        let output = '';
+
+        $.each(response.results, function (key, result) {
+            output += `
+            <div class="col-md-4 mb-3 d-flex">
+                <div class="card text-center mb-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <p class="card-text">${result.name}</p>
+                        <a href="${result.url}" class="btn btn-primary">Detail</a>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+        $('#pokemon-container').html(output);
+    }
+});
